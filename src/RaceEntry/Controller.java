0b583ee8d3ab19c33e77e5f6f857cdbbb132988c
Nitrax <Nitrax92@gmail.com>
2016-12-22 19:56:08 +0100

    OpenCSV for reading files. Search in csv by line.

--- src/RaceEntry/Controller.java
+++ src/RaceEntry/Controller.java
@@ -1,11 +1,20 @@
 package RaceEntry;
 
+import com.opencsv.CSVReader;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Scanner;
+
+
+// OpenCSV
+
 
 /**
  * Created by Mello on 20.12.2016.
@@ -13,7 +22,7 @@ import java.util.Scanner;
 public class Controller {
 
     @FXML TextField session_name = new TextField();
-
+    @FXML ChoiceBox TyreChoice = new ChoiceBox();
     @FXML
     public void nextButton(ActionEvent event) throws IOException{
         System.out.println("*******************************");
@@ -49,6 +58,31 @@ public class Controller {
             e.printStackTrace();
         }
 
+    }
+
+
+    @FXML
+    public void searchButton(ActionEvent event) throws IOException, InvocationTargetException{
+        System.out.println("**************************** Line Search ************************");
+
+        String filename = "saved_data\\01.csv";
+        File file = new File(filename);
+        CSVReader reader = new CSVReader(new FileReader(file));
+        String [] nextLine;
+
+
+
+
+        while ((nextLine = reader.readNext()) != null) {
+            if(nextLine != null){
+                String s = Arrays.toString(nextLine);
+                if(nextLine[0].equals("S")){
+                    System.out.println("Hit!");
+                    System.out.println(Arrays.toString(nextLine));
+                }
+            }
+
+        }
     }
 
 
